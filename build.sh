3d0003aed311e3a73e2c9065db76fc4d76d2f2c3
Vincenzo Fabrizi <vfabrizi@thoughtworks.com>
2016-11-30 15:14:05 +0000

    vagrant box

---
+++ build.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/Appdynamics/dropwizard-sample-app
+git clone https://github.com/ChrisJTaylor/java-sandbox
+
+/opt/maven/apache-maven-3.3.9/bin/mvn package -f ~/dropwizard-sample-app/
+
+#docker build -f ./Dockerfile.ubuntu -t ubuntu/dropwizard-example .
+#docker run -p 8082:8082 -p 8083:8083 -d ubuntu/dropwizard-example:latest
+
+docker build -f ./Dockerfile.alpine -t alpine/dropwizard-example .
+docker run -p 8082:8082 -p 8083:8083 -d alpine/dropwizard-example:latest

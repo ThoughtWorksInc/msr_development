9922f9c5ff53fd17393c21a5cba3dcab1512f6b4
Mohammed Binsabbar <ibinsabbar@gmail.com>
2016-12-07 14:21:38 +0000

    Mike/Mo #121: Moved Vagrant initialisation to separate script file

--- init_dev.sh
+++ init_dev.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
 
+source scripts/vagrant_setup.sh
 
-function download_vagrant() {
-	VAGRANT_DOWNLOAD_URL="https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0.dmg"
-	wget $VAGRANT_DOWNLOAD_URL -O ~/Desktop/$(basename $VAGRANT_DOWNLOAD_URL)
-	echo "Vagrant is downloaded to your Desktop. Install it, and rerun this script"
-	exit 0
-}
-
-if ! which vagrant > /dev/null; then
-	echo "Downloading Vagrant"
-	download_vagrant
-fi
-
-VAGRANT_VERSION=$(vagrant -v | grep -Eo "\d+\.\d+")                                                      
-if [[ "$VAGRANT_VERSION" != "1.9" ]]; then                                                                                                                                       
-    echo "current Vagrant version is: $VAGRANT_VERSION. Downloading Vagrant 1.9"
-    download_vagrant                                                                                                                              
-fi
+init_vagrant

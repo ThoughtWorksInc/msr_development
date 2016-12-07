f5d4e7257ee1222ba89b7e09b9fa632f5be123f5
Mohammed Binsabbar <ibinsabbar@gmail.com>
2016-12-07 12:09:32 +0000

    Mike/Mo #121: Download and check vagrant version

--- init_dev.sh
+++ init_dev.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -e
 
-vagrant_version=$(vagrant -v | grep -Eo "\d+\.\d+")                                                                                     
-if [[ "$vagrant_version" != "1.9" ]]; then                                                                                                                                       
-    echo "current Vagrant version is: $vagrant_version. Upgrade vagrant to 1.9"                                                                                                                               
-    exit -1
-fi         
 
-#TODO clone to repos
+function download_vagrant() {
+	VAGRANT_DOWNLOAD_URL="https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0.dmg"
+	wget $VAGRANT_DOWNLOAD_URL -O ~/Desktop/$(basename $VAGRANT_DOWNLOAD_URL)
+	echo "Vagrant is downloaded to your Desktop. Install it, and rerun this script"
+	exit 0
+}
+
+if ! which vagrant > /dev/null; then
+	echo "Downloading Vagrant"
+	download_vagrant
+fi
+
+VAGRANT_VERSION=$(vagrant -v | grep -Eo "\d+\.\d+")                                                      
+if [[ "$VAGRANT_VERSION" != "1.9" ]]; then                                                                                                                                       
+    echo "current Vagrant version is: $VAGRANT_VERSION. Downloading Vagrant 1.9"
+    download_vagrant                                                                                                                              
+fi

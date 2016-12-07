d0fed54c6fccfb3d9db670aa2ea0ddd47aa9cd89
Mohammed Binsabbar <ibinsabbar@gmail.com>
2016-12-07 15:46:23 +0000

    tidy up bootstartp and Vagrantfile

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+echo -e "\nUpdating the system..."
+apt-get update -y
+
+echo "Installing OpenJDK8..."
+add-apt-repository -y ppa:openjdk-r/profilepa 2>&1
+apt-get update
+apt-get install -y openjdk-8-jdk
+update-alternatives --config java
+update-alternatives --config javac
+
+echo "Installing git, unzip and docker..."
+apt-get install -y git unzip docker.io
+
+echo -e "\nInstalling Gradle..."
+mkdir -p /usr/local/opt/gradle
+if which gradle 2> /dev/null ; then
+	gradle --stop 2>&1
+fi
+wget https://services.gradle.org/distributions/gradle-3.2.1-bin.zip -O /tmp/gradle-3.2.1-bin.zip --no-verbose
+unzip -o /tmp/gradle-3.2.1-bin.zip -d /usr/local/opt/gradle
+cat > /etc/profile.d/gradle.sh << 'EOF'
+export PATH=$PATH:/usr/local/opt/gradle/gradle-3.2.1/bin
+EOF

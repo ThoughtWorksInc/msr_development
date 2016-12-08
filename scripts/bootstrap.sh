80fd5ad6ad9e71b08530acce6274897af01338c0
Mike Winter <mwinter@thoughtworks.com>
2016-12-08 12:45:12 +0000

    Mike/Medina #121: Specified versions of installed packages

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $FAST_PROVISION == "true" ]]; then
-	echo "Fast provision is done"
-	exit 0
-fi
-
-export DEBIAN_FRONTEND=noninteractive
-
 echo -e "\nUpdating the system..."
 apt-get update -y
 
 echo "Installing OpenJDK8..."
 add-apt-repository -y ppa:openjdk-r/ppa 2>&1
 apt-get update
-apt-get install -y openjdk-8-jdk
+apt-get install -y openjdk-8-jdk=8u111-b14-3~14.04.1
 update-alternatives --config java
 update-alternatives --config javac
 
 echo "Installing git, unzip and docker..."
-apt-get install -y git unzip docker.io
-
-sudo groupadd docker
-sudo gpasswd -a vagrant docker
+apt-get install -y git=1:1.9.1-1ubuntu0.3 unzip=6.0-9ubuntu1.5 docker.io=1.6.2~dfsg1-1ubuntu4~14.04.1
